8395dae12250fd917b524058a12cea8f20a31817
Mark <MarkKranenburg@hotmail.nl>
2023-01-23 22:01:21 +0100

    add psudocode

---
+++ script.js
@@ -0,0 +1,44 @@
+/* 
+1. Understand the problem
+        Er moet backendcode geschreven worden voor het spel.
+
+
+2. plan
+    a. user interface?
+            Komt later.
+
+    b. input?
+            De input moet van de user komen. dit kan in het in het
+            begin met een alert maar hier moet later een mooie UI voor
+            komen. 
+
+    c. desired output?
+            de backend code voor het spel. de user heeft 3 keuzes 
+            rock, paper en scissors. deze keuze moet vergeleken worden
+            met de keuze van de pc. zo wordt er bepaald wie deze ronde
+            heeft gewonnen. er komen 3 rondes? best of 3?
+
+    d. necessary steps to get output?
+            1. mannier voor user om input te geven.
+            2. mannier voor pc om optie te kiezen. 
+            3. mannier om de opties te vergelijken en winnen voor ronde kiezen
+            4. mannier om user te laten zien wie de ronde gewonnen heeft
+            5. mannier om rondes bij te houden. en de winnaar uit 3 te kiezen
+            6. mannier om winnaar te laten zien. 
+            7. mannier om spel opnieuw te spelen.
+3. psudocode
+
+
+button voor het starten van een game
+bericht ronde 1
+get user imput en sla op in variable
+functie voor het keuze pc, sla op in variable
+functie voor het vergelijken van keuze user en keuze pc
+bepaal winnaar
+winnaar krijg punt bij score 
+er wordt punt opgeteld bij aantal rondes. 
+bepaal wie meeste punten en sla op in variable
+if (meeste punten < 5) {nieuwe ronden}
+esle bericht winnaar, en geef optie voor nieuw spel. 
+
+*/
