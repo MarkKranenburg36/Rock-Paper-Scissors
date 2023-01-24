526c7b40c7ec35a48a3b6f4d019e38a29c508af2
Mark <MarkKranenburg@hotmail.nl>
2023-01-24 23:10:06 +0100

    add function to compaire playerSelection to computerSelection

--- script.js
+++ script.js
@@ -32,7 +32,7 @@
 button voor het starten van een game
 bericht ronde 1
 get user imput en sla op in variable
-functie voor het keuze pc, sla op in variable
+*functie voor het keuze pc, sla op in variable
 functie voor het vergelijken van keuze user en keuze pc
 bepaal winnaar
 winnaar krijg punt bij score 
@@ -43,6 +43,41 @@ esle bericht winnaar, en geef optie voor nieuw spel.
 
 */
 
+//get user input 
+
+let playerSelection = "rock";
+playerSelection = playerSelection.toLocaleLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+        switch (playerSelection, computerSelection){
+                case (playerSelection === 0 && computerSelection === 0):
+                        return 'Its a tie! You both picked rock, seems like you\'re both rock-solid.';
+                        break;
+                case (playerSelection === 0 && computerSelection === 1):
+                        return 'You lose! A rock ain\'t always a hard place, sometimes paper can cover it!';
+                        break;
+                case (playerSelection === 0 && computerSelection === 2):
+                        return 'You win! Rock and roll baby!';
+                        break;
+                case (playerSelection === 1 && computerSelection === 0):
+                        return 'You win! Looks like the computer just got schooled, paper always wins.';
+                        break;
+                case (playerSelection === 1 && computerSelection === 1):
+                        return 'It\'s a tie! You both picked paper, but let\'s be real, paper is the best choice anyway.';
+                        break;
+                case (playerSelection === 1 && computerSelection === 2):
+                        return 'You lose! Looks like paper just met its mortal enemy, scissors always wins!';
+                        break;
+                case (playerSelection === 2 && computerSelection === 0):
+                        return 'You lose! But I bet the rock band next door is loving this!';
+                        break;
+                case (playerSelection === 2 && computerSelection === 1):
+                        return 'You win! Looks like the computer got cut down to size!';
+                        break;
+                case (playerSelection === 2 && computerSelection === 2):
+                        return 'It\'s a tie! You both picked scissorts, seems like you both have a sharp mind and cutting edge strategy!';
+                        break;
+}}
 
 
 //functie voor het maken van de computer's keuze
@@ -59,4 +94,5 @@ function getComputerChoice(randomChoice) {
                 return "Scissors";
         }
 }
+
 console.log(getComputerChoice(randomChoice));
