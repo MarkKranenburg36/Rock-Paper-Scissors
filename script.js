725ae4d89e8711d11abe9d258c53172faf29e2d5
Mark <MarkKranenburg@hotmail.nl>
2023-01-25 21:13:56 +0100

    make two functions be able to work together

--- script.js
+++ script.js
@@ -48,33 +48,36 @@ esle bericht winnaar, en geef optie voor nieuw spel.
 let playerSelection = "rock";
 playerSelection = playerSelection.toLocaleLowerCase();
 
+
+
+
 function playRound(playerSelection, computerSelection) {
         switch (playerSelection, computerSelection){
-                case (playerSelection === 0 && computerSelection === 0):
+                case (playerSelection === 'rock' && computerSelection === 'rock'):
                         return 'Its a tie! You both picked rock, seems like you\'re both rock-solid.';
                         break;
-                case (playerSelection === 0 && computerSelection === 1):
+                case (playerSelection === 'rock' && computerSelection === 'paper'):
                         return 'You lose! A rock ain\'t always a hard place, sometimes paper can cover it!';
                         break;
-                case (playerSelection === 0 && computerSelection === 2):
+                case (playerSelection === 'rock' && computerSelection === 'scissors'):
                         return 'You win! Rock and roll baby!';
                         break;
-                case (playerSelection === 1 && computerSelection === 0):
+                case (playerSelection === 'paper' && computerSelection === 'rock'):
                         return 'You win! Looks like the computer just got schooled, paper always wins.';
                         break;
-                case (playerSelection === 1 && computerSelection === 1):
+                case (playerSelection === 'paper' && computerSelection === 'paper'):
                         return 'It\'s a tie! You both picked paper, but let\'s be real, paper is the best choice anyway.';
                         break;
-                case (playerSelection === 1 && computerSelection === 2):
+                case (playerSelection === 'paper' && computerSelection === 'scissors'):
                         return 'You lose! Looks like paper just met its mortal enemy, scissors always wins!';
                         break;
-                case (playerSelection === 2 && computerSelection === 0):
+                case (playerSelection === 'scissors' && computerSelection === 'rock'):
                         return 'You lose! But I bet the rock band next door is loving this!';
                         break;
-                case (playerSelection === 2 && computerSelection === 1):
+                case (playerSelection === 'scissors' && computerSelection === 'paper'):
                         return 'You win! Looks like the computer got cut down to size!';
                         break;
-                case (playerSelection === 2 && computerSelection === 2):
+                case (playerSelection === 'scissors' && computerSelection === 'scissors'):
                         return 'It\'s a tie! You both picked scissorts, seems like you both have a sharp mind and cutting edge strategy!';
                         break;
 }}
@@ -85,14 +88,12 @@ let randomChoice = Math.floor((Math.random() * 3));
 
 function getComputerChoice(randomChoice) {
         if (randomChoice === 0) {
-                return "Rock";
+                return "rock";
         } 
         else if(randomChoice === 1) {
-                return "Paper";
+                return "paper";
         }
         else if (randomChoice === 2) {
-                return "Scissors";
+                return "scissors";
         }
 }
-
-console.log(getComputerChoice(randomChoice));
