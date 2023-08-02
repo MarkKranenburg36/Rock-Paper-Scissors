b7032c617feb2102dab3b53416d64f288901c55b
Mark <MarkKranenburg@hotmail.nl>
2023-08-02 16:17:03 +0200

    add buttons + event listeners

--- script.js
+++ script.js
@@ -1,109 +1,66 @@
-/* 
-1. Understand the problem
-        Er moet backendcode geschreven worden voor het spel.
-
-
-2. plan
-    a. user interface?
-            Komt later.
-
-    b. input?
-            De input moet van de user komen. dit kan in het in het
-            begin met een alert maar hier moet later een mooie UI voor
-            komen. 
-
-    c. desired output?
-            de backend code voor het spel. de user heeft 3 keuzes 
-            rock, paper en scissors. deze keuze moet vergeleken worden
-            met de keuze van de pc. zo wordt er bepaald wie deze ronde
-            heeft gewonnen. er komen 3 rondes? best of 3?
-
-    d. necessary steps to get output?
-            1. mannier voor user om input te geven.
-            2. mannier voor pc om optie te kiezen. 
-            3. mannier om de opties te vergelijken en winnen voor ronde kiezen
-            4. mannier om user te laten zien wie de ronde gewonnen heeft
-            5. mannier om rondes bij te houden. en de winnaar uit 3 te kiezen
-            6. mannier om winnaar te laten zien. 
-            7. mannier om spel opnieuw te spelen.
-3. psudocode
-
-
-button voor het starten van een game
-bericht ronde 1
-get user imput en sla op in variable
-*functie voor het keuze pc, sla op in variable
-functie voor het vergelijken van keuze user en keuze pc
-bepaal winnaar
-winnaar krijg punt bij score 
-er wordt punt opgeteld bij aantal rondes. 
-bepaal wie meeste punten en sla op in variable
-if (meeste punten < 5) {nieuwe ronden}
-esle bericht winnaar, en geef optie voor nieuw spel. 
-
-*/
-
-//get user input 
-
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound(playerSelection, computerSelection) {
-        if (playerSelection === 'rock' && computerSelection === 'rock')
+const rock = document.querySelector('#btn-rock');
+const paper = document.querySelector('#btn-paper');
+const scissors = document.querySelector('#btn-scissors');
+
+rock.addEventListener('click', () => {
+        let playersChoice = event.target.textContent;
+        console.log(playersChoice);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let getComputerChoice = () => {
+        let random = Math.floor(Math.random() * 3) + 1;
+        if (random === 1){
+                return 'Rock';
+        } else if (random === 2){
+                return 'Paper';
+        } else if (random === 3){
+                return 'Scissors';
+        }
+}
+
+function playRound(playerSelection, getComputerChoice) {
+        if (playerSelection === 'rock' && getComputerChoice() === 'rock') {
          return 'Its a tie! You both picked rock, seems like you\'re both rock-solid.';
-                
-        else if (playerSelection === 'rock' && computerSelection === 'paper')
+        }
+        else if (playerSelection === 'rock' && getComputerChoice() === 'paper'){
         return 'You lose! A rock ain\'t always a hard place, sometimes paper can cover it!';
                 playerScore++;
-        else if (playerSelection === 'rock' && computerSelection === 'scissors')
+        }
+        else if (playerSelection === 'rock' && getComputerChoice() === 'scissors'){
         return 'You win! Rock and roll baby!';
-        else if (playerSelection === 'paper' && computerSelection === 'rock')
-        return 'You win! Looks like the computer just got schooled, paper always wins.';
-        else if (playerSelection === 'paper' && computerSelection === 'paper')
+        }
+        else if (playerSelection === 'paper' && getComputerChoice() === 'rock'){
+                return 'You win! Looks like the computer just got schooled, paper always wins.';
+        }
+        else if (playerSelection === 'paper' && getComputerChoice() === 'paper'){
         return 'It\'s a tie! You both picked paper, but let\'s be real, paper is the best choice anyway.';
-        else if (playerSelection === 'paper' && computerSelection === 'scissors')
+        }
+        else if (playerSelection === 'paper' && getComputerChoice() === 'scissors'){
         return 'You lose! Looks like paper just met its mortal enemy, scissors always wins!';
-        else if (playerSelection === 'scissors' && computerSelection === 'rock')
+        }
+        else if (playerSelection === 'scissors' && getComputerChoice() === 'rock'){
         return 'You lose! But I bet the rock band next door is loving this!';
-        else if (playerSelection === 'scissors' && computerSelection === 'paper')
+        }
+        else if (playerSelection === 'scissors' && getComputerChoice() === 'paper'){
         return 'You win! Looks like the computer got cut down to size!';
+        }
         else
-        return 'It\'s a tie! You both picked scissorts, seems like you both have a sharp mind and cutting edge strategy!........defealt';
-}
-
-
-
-//functie voor het maken van de computer's keuze
-let randomChoice;
-
-function getComputerChoice() {
-        if (randomChoice === 0) {
-                return 'rock';
-        } 
-        else if(randomChoice === 1) {
-                return 'paper';
-        }
-        else {
-                return 'scissors';
-        }
-}
-
-
-let playerSelection;
-let computerSelection;
-
-
-function game() {
-        for (let i=0; i <=4; i++) {
-                randomChoice = Math.floor((Math.random() * 3));
-                computerSelection = getComputerChoice(randomChoice);
-                playerSelection = prompt("Choose rock, paper or scissors!");
-                playerSelection = playerSelection.toLocaleLowerCase();
-                playRound(playerSelection, computerSelection);
-        }
-
-        // if playerscore > computerscore player wins
-
-}
-
-game(playRound(playerSelection, computerSelection));
+        return 'It\'s a tie! You both picked scissorts, seems like you both have a sharp mind and cutting edge strategy!';
+};
